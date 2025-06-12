7b16c0149ed63446f87e5709067dc286a2c24bea
nidhi-vm <Nidhi.Vasani@ca.ey.com>
2025-06-12 14:04:10 -0400

    Updating using GitHub API 2025-06-12-14:04

--- src/test/java/com/business/BusinessProjectApplicationTests.java
+++ src/test/java/com/business/BusinessProjectApplicationTests.java
@@ -8,6 +8,6 @@ class BusinessProjectApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// This method is intentionally left empty. It is used to test if the application context loads successfully.
 	}
-
 }
