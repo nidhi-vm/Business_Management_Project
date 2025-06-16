b73ffb3b1997803b69b635c993fdba142e3eaece
nidhi-vm <Nidhi.Vasani@ca.ey.com>
2025-06-16 14:56:34 -0400

    Updating using GitHub API 2025-06-16-14:56

--- src/main/java/com/business/controllers/UserController.java
+++ src/main/java/com/business/controllers/UserController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.business.entities.User;
 import com.business.services.UserServices;
@@ -13,31 +15,31 @@ import com.business.services.UserServices;
 @Controller
 public class UserController
 {
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+	private static final String REDIRECT_SERVICES = "redirect:/admin/services";
+
 	@Autowired
 	private UserServices services;
 
 	@PostMapping("/addingUser")
-	public String  addUser(@ModelAttribute User user)
+	public String addUser(@ModelAttribute User user)
 	{
-		System.out.println(user);
+		logger.info("User added: {}", user);
 		this.services.addUser(user);
-		return "redirect:/admin/services";
+		return REDIRECT_SERVICES;
 	}
 
 	@GetMapping("/updatingUser/{id}")
 	public String updateUser(@ModelAttribute User user, @PathVariable("id") int id)
 	{
 		this.services.updateUser(user, id);
-		return "redirect:/admin/services";
+		return REDIRECT_SERVICES;
 	}
 
 	@GetMapping("/deleteUser/{id}")
-	public String deleteUser(@PathVariable("id" )int id)
+	public String deleteUser(@PathVariable("id") int id)
 	{
 		this.services.deleteUser(id);
-		return "redirect:/admin/services";
+		return REDIRECT_SERVICES;
 	}
-
-
-
 }
