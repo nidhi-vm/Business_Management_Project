d8b6d409cbffac7cb2982ccbea5c28d6f14bcbc3
nidhi-vm <Nidhi.Vasani@ca.ey.com>
2025-06-16 14:56:46 -0400

    Updating using GitHub API 2025-06-16-14:56

--- src/main/java/com/business/Exceptions.java
+++ src/main/java/com/business/Exceptions.java
@@ -1,5 +1,7 @@
 package com.business;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -8,15 +10,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ControllerAdvice
 public class Exceptions
 {
+	private static final Logger logger = LoggerFactory.getLogger(Exceptions.class);
 
 	@ResponseStatus(value=HttpStatus.INTERNAL_SERVER_ERROR)
-
 	@ExceptionHandler(value=Exception.class) 
 	public String handler()
 	{
-		System.out.println("Exception Handled....!!!!");
+		logger.error("Exception Handled....!!!!");
 		return "exception"; 
 	}
-
-
 }
