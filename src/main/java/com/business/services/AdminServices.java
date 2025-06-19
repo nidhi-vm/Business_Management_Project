3f986ca9634a47261c72cce09bdb085bb0c6a730
nidhi-vm <Nidhi.Vasani@ca.ey.com>
2025-06-19 10:44:05 -0400

    Updating using GitHub API 2025-06-19-10:44

--- src/main/java/com/business/services/AdminServices.java
+++ src/main/java/com/business/services/AdminServices.java
@@ -11,54 +11,49 @@ import com.business.repositories.AdminRepository;
 @Component
 public class AdminServices
 {
-	@Autowired
-	private AdminRepository adminRepository;
-	
-	//Get All Admins
-	public List<Admin>getAll()
-	{
-		 List<Admin> admins = (List<Admin>)this.adminRepository.findAll();
-		 return admins;
-	}
-	//Get Single Admin
-	public Admin getAdmin(int id)
-	{
-		Optional<Admin> optional = this.adminRepository.findById(id);
-		Admin admin=optional.get();
-		return admin;
-	}
-//Update Admin
-	public void update(Admin admin ,int id)
-	{
-		for (Admin ad : getAll()) 
-		{
-			if(ad.getAdminId()==id)
-			{
-				this.adminRepository.save(admin);
-			}
-		}
-	}
-	
-	//delete User
-	public void delete(int id)
-	{
-		this.adminRepository.deleteById(id);
-	}
-	
-	//add Admin
-	public void addAdmin(Admin admin)
-	{
-		this.adminRepository.save(admin);
-	}
-	
-	//Validating Admin login
-	public boolean validateAdminCredentials(String email,String password)
-	{
-		Admin admin=adminRepository.findByAdminEmail(email);
-		if(admin!=null && admin.getAdminPassword().equals(password))
-		{
-			return true;
-		}
-		return false;
-	}
+    @Autowired
+    private AdminRepository adminRepository;
+    
+    //Get All Admins
+    public List<Admin> getAll()
+    {
+        return (List<Admin>) this.adminRepository.findAll();
+    }
+    
+    //Get Single Admin
+    public Admin getAdmin(int id)
+    {
+        return this.adminRepository.findById(id).orElse(null);
+    }
+    
+    //Update Admin
+    public void update(Admin admin, int id)
+    {
+        for (Admin ad : getAll()) 
+        {
+            if (ad.getAdminId() == id)
+            {
+                this.adminRepository.save(admin);
+            }
+        }
+    }
+    
+    //delete User
+    public void delete(int id)
+    {
+        this.adminRepository.deleteById(id);
+    }
+    
+    //add Admin
+    public void addAdmin(Admin admin)
+    {
+        this.adminRepository.save(admin);
+    }
+    
+    //Validating Admin login
+    public boolean validateAdminCredentials(String email, String password)
+    {
+        Admin admin = adminRepository.findByAdminEmail(email);
+        return admin != null && admin.getAdminPassword().equals(password);
+    }
 }
