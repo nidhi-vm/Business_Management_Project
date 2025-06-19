b977cab4291d3a804130cb751fee77c3695aba26
nidhi-vm <Nidhi.Vasani@ca.ey.com>
2025-06-19 10:44:11 -0400

    Updating using GitHub API 2025-06-19-10:44

--- src/main/java/com/business/loginCredentials/UserLogin.java
+++ src/main/java/com/business/loginCredentials/UserLogin.java
@@ -1,30 +1,31 @@
-package com.business.loginCredentials;
+package com.business.logincredentials;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 
 public class UserLogin 
 {
+    private String userEmail;
+    private String userPassword;
 
-private String userEmail;
-private String userPassword;
-public String getUserEmail() {
-	return userEmail;
-}
-public void setUserEmail(String userEmail) {
-	this.userEmail = userEmail;
-}
-public String getUserPassword() {
-	return userPassword;
-}
-public void setUserPassword(String userPassword) {
-	this.userPassword = userPassword;
-}
-@Override
-public String toString() {
-	return "UserLogin [userEmail=" + userEmail + ", userPassword=" + userPassword + "]";
-}
+    public String getUserEmail() {
+        return userEmail;
+    }
 
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
 
+    public String getUserPassword() {
+        return userPassword;
+    }
 
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    @Override
+    public String toString() {
+        return "UserLogin [userEmail=" + userEmail + ", userPassword=" + userPassword + "]";
+    }
 }
